1aecad935670453d850e3eb22fae384d791a61f8
unknown <isururaveen4520@gmail.com>
2021-04-19 22:04:39 +0530

    Single buyer information retrieving service added

--- Buyer/src/main/java/model/Buyer.java
+++ Buyer/src/main/java/model/Buyer.java
@@ -41,6 +41,7 @@ public class Buyer {
 				
 				Connection con = connect();
 				
+				//Check DB Conenction
 				if(con == null)
 				{
 					return "Unable to Connect Database for Retreive Data";
@@ -99,4 +100,69 @@ public class Buyer {
 			return output;
 		}
 		
+		//Retrieve Single Buyer Information------------------------------------------------------------------------
+		public String getBuyer(String buyerID) 
+		{
+			
+			String output = "";
+			
+			try 
+			{
+					
+				Connection con = connect();
+				
+				if(con == null)
+				{
+					return "Unable to Connect Database for Retreive "+ buyerID +"";
+				}
+				
+				//Prepare the HTML table to be displayed Single Buyer
+				output = "<table border ='1'><tr><th> Buyer ID </th>"+
+						 "<th> First Name </th>"+
+						 "<th> Last Name </th>"+
+						 "<th> Address </th>"+
+						 "<th> Email </th>"+
+						 "<th> Phone No </th>"+
+						 "<th> User Name </th>"+
+						 "<th> Password </th>";
+				
+				int val = Integer.parseInt(buyerID);
+				
+				String sqlQuery = "SELECT * FROM buyers WHERE buyerID="+val;
+				Statement statement = con.createStatement();
+				
+				ResultSet rs = statement.executeQuery(sqlQuery);
+				
+				if(rs.next()) 
+				{
+					String buyID = Integer.toString(rs.getInt("buyerID"));
+					String firstName = rs.getString("firstName");
+					String lastName = rs.getString("lastName");
+					String address = rs.getString("address");
+					String email = rs.getString("email");
+					String phoneNo = rs.getString("phoneNo");
+					String userName = rs.getString("userName");
+					String password = rs.getString("password");
+					
+					output += "<tr><td>" + buyID + "</td>";
+					output += "<td>" + firstName + "</td>";
+					output += "<td>" + lastName + "</td>";
+					output += "<td>" + address + "</td>";
+					output += "<td>" + email + "</td>";
+					output += "<td>" + phoneNo + "</td>";
+					output += "<td>" + userName + "</td>";
+					output += "<td>" + password + "</td>";
+				}
+				con.close();
+				output += "</table>";
+				
+				return output;
+				
+			}catch(Exception ex) {
+				output = "Unable to get "+ buyerID +"";
+				ex.printStackTrace();
+			}
+			
+			return output;
+		}
 }
