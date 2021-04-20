6e03a2e7ae6760a596c412777bce6b890b59c3bb
unknown <isururaveen4520@gmail.com>
2021-04-20 18:48:54 +0530

    Buyer registration service added

--- Buyer/src/main/java/com/BuyerService.java
+++ Buyer/src/main/java/com/BuyerService.java
@@ -53,4 +53,45 @@ public class BuyerService {
 		System.out.println(buyerID);
 		return buyer.getBuyer(buyerID);
 	}
+
+	//Buyer Registration Service Access
+	@POST
+	@Path("register")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String CreateBuyer(@FormParam("firstName") String firstName, 
+			@FormParam("lastName") String lastName,
+			@FormParam("address") String address, 
+			@FormParam("email") String email, 
+			@FormParam("phoneNo") String phoneNo, 
+			@FormParam("userName") String userName, 
+			@FormParam("password")String password)
+	{
+		
+		String output="";
+		
+		if(firstName.length() == 0 || lastName.length() == 0 || address.length() == 0 || email.length() == 0 || phoneNo.length() == 0 || userName.length() == 0 || password.length() == 0)
+		{
+			return "You Cannot keep Mendotory Fields Empty..! Please Try Again..!";
+		}
+		else 
+		{
+			if(userName.length()>3)
+			{
+				return "User Name Should Have More Than 3 Digits..!"; 
+			}
+			else 
+			{
+				if(password.length()<4)
+				{
+					return "Please Provide Password Which has more than 4 Digits..!";
+				}
+				else
+				{
+					output = buyer.insertBuyer(firstName, lastName, address, email, phoneNo, userName, password);
+					return output;
+				}
+			}
+		}
+	}
 }
