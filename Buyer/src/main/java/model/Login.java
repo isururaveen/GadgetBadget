dc2eea97cf8e64058a3f43809c62aac2d47199e0
unknown <isururaveen4520@gmail.com>
2021-04-21 22:24:34 +0530

    Forget Password function added

--- Buyer/src/main/java/model/Login.java
+++ Buyer/src/main/java/model/Login.java
@@ -94,5 +94,86 @@ public class Login {
 		}
 		return null;
 	}
+
+	//ForgetPassword
+	public String forgotPassword(String userName, String password)
+	{
+		String query1 = new String("SELECT * FROM buyers WHERE userName =? AND password=?");
+		String query2 = new String("SELECT * FROM researchers WHERE userName=? AND password=?");
+		String query3 = new String("SELECT * FROM funding WHERE userName=? AND password=?");
+		
+		try
+		{
+			
+			Connection con = connect();
+			
+			if(con == null)
+			{
+				return "Unable to Connect Database for Retreive Data!";
+			}
+			
+			PreparedStatement stmt = con.prepareStatement(query1);
+			stmt.setString(1, userName);
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next())
+			{
+				String query4 = new String("UPDATE buyers SET password=? WHERE userName=?");
+				PreparedStatement stmt1 = con.prepareStatement(query4);
+				
+				stmt1.setString(1, password);
+				stmt1.setString(2, userName);
+				
+				stmt1.executeUpdate();
+				return "Buyer Password Changed!";
+			
+			}else if(!rs.next())
+			{
+				
+				PreparedStatement stmt2 = con.prepareStatement(query2);
+				stmt2.setString(1, password);
+				ResultSet rs1 = stmt2.executeQuery();
+				
+				if(!rs1.next())
+				{
+					String query5 = new String("UPDATE researchers SET password=? WHERE userName=?");
+					PreparedStatement stmt3 = con.prepareStatement(query5);
+					
+					stmt3.setString(1, password);
+					stmt3.setString(2, userName);
+					
+					stmt3.executeUpdate();
+					return "Researcher Password Changed!";
+					
+				}else if(!rs1.next())
+				{
+					PreparedStatement stmt3 = con.prepareStatement(query3);
+					stmt3.setString(1, userName);
+					ResultSet rs2 = stmt3.executeQuery();
+					
+					if(rs1.next())
+					{
+						
+						String query6 = new String("UPDATE funding SET password=? WHERE userName=?");
+						PreparedStatement stmt4 = con.prepareStatement(query6);
+						
+						stmt4.setString(1, password);
+						stmt4.setString(2, userName);
+						
+						stmt4.executeUpdate();
+						return "Funding Password Changed!";
+					}
+					else {
+						return "User Name you entered doesn't exist";
+					}
+				}
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
 
